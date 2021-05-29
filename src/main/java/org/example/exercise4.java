ec9d6bd31dcb98de78f024d0681f984d2aa7294c
Drake Scott <drakescott999@hotmail.com>
2021-05-29 18:20:58 -0400

    edited code to only use 1 scanner

--- src/main/java/org/example/exercise4.java
+++ src/main/java/org/example/exercise4.java
@@ -4,21 +4,18 @@ import java.util.Scanner;
 public class exercise4 {
     public static void main(String[] args){
 
-        Scanner str1 = new Scanner (System.in);
+        Scanner str = new Scanner (System.in);
         System.out.print("Enter a noun: ");
-        String noun = str1.nextLine();
+        String noun = str.nextLine();
 
-        Scanner str2 = new Scanner (System.in);
         System.out.print("Enter a verb: ");
-        String verb = str2.nextLine();
+        String verb = str.nextLine();
 
-        Scanner str3 = new Scanner (System.in);
         System.out.print("Enter an adjective: ");
-        String adj = str3.nextLine();
+        String adj = str.nextLine();
 
-        Scanner str4 = new Scanner (System.in);
         System.out.print("Enter an adverb: ");
-        String adv = str4.nextLine();
+        String adv = str.nextLine();
 
         System.out.println("Do you " + verb + " your " + adj + " " + noun + " " + adv + "? That's hilarious!");
 
